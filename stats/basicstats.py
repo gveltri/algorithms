733f3e8ee97a2318f522675745afd53a5344bdf3
Gerardo Veltri <gerardo.veltri@gmail.com>
2015-11-23 19:50:24 -0500

    cleaned up a bit

--- stats/basicstats.py
+++ stats/basicstats.py
@@ -2,27 +2,29 @@ class BasicStats:
 
     def __init__(self, array):
         self.array = array
-        print 'length: ' + str(len(self.array))
-
-    def findMedian(self):
+        self.array.sort()
+        
+    def printValues(self):
+        print 'size: ' + str(len(self.array))
+        print 'median: ' + str(self.Median())
+        print 'mode: ' + str(self.Mode())
+        print 'mean: ' + str(self.Mean())
+        
+    def Median(self):
         array = self.array
-        array.sort()
         length = len(array)
         median = 0
         if length%2!=0:
             median = array[length/2]
         else:
             median = float(array[length/2] + array[(length/2)-1])/2
-
-        print "median: " + str(median)
         return median
 
-    def findMean(self):
+    def Mean(self):
         mean = float(sum(self.array))/len(self.array)
-        print "mean: " + str(mean)
         return mean
 
-    def findMode(self):
+    def Mode(self):
         unique_nums = {}
         max_val = 0
         max_key = None
@@ -37,11 +39,7 @@ class BasicStats:
                 if unique_nums[i] > max_val:
                     max_val = unique_nums[i]
                     max_key = i
-        print "mode: " + str(max_key)
         return max_key
-        
 
-obj = BasicStats([8,8,1,7,8,1,7,4])
-obj.findMedian()
-obj.findMean()
-obj.findMode()
+obj = BasicStats([6,9,7,10,5,3,3,2])
+obj.printValues()
